af3e30a0130df3bd249332a3f258b64a856a5ac7
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-12-04 16:30:52 -0800

    added student.h

---
+++ student.h
@@ -0,0 +1,17 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+        private:
+                string firstName;
+                string lastName;
+               // int number;
+               // student person;
+
+        public:
+                void setName(string string);
+                string fullName();
+};
+
+#endif
